6f7895732e9194f563d799b6a7ab6b7cb9bb5075
Andrey1303110 <andrey1303110@gmail.com>
2021-02-16 20:02:18 +0200

    add folder products

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(".head_slide").click(function(){
     console.log(this.id);
     setTimeout(() => { 
         $(".question_text", this).css("font-size", "0%");
-        setTimeout(() => { $("#" + this.id).css("background-image", `url(./images/${this.id}.png)`) }, 1200 )
+        setTimeout(() => { $("#" + this.id).css("background-image", `url(./images/products/${this.id}.png)`) }, 1200 )
     }, 7500 )
 });
 
